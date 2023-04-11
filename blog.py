ef80e83598b55f40609bc62b2e6545843d08824a
Bilge Kağan Börekçi <bilgekaganbr@gmail.com>
2023-04-12 00:26:40 +0300

    Second Commit

--- blog.py
+++ blog.py
@@ -6,6 +6,7 @@ from passlib.hash import sha256_crypt
 #User Registration Form
 class RegisterForm(Form):
 
+    #fields with their names and validators
     name = StringField("Name Surname", validators=[validators.Length(min=4, max=25)])
     username = StringField("Username", validators=[validators.Length(min=5, max=35)])
     email = StringField("Email", validators=[validators.Email(message="Please enter a valid email address...")])
@@ -13,6 +14,7 @@ class RegisterForm(Form):
                                                      validators.EqualTo(fieldname="confirm", message="Password does not match")])
     confirm = PasswordField("Confirm Password")
 
+#define and config the Flask app
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
@@ -20,22 +22,23 @@ app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = "ybblog"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
+#create a MySQL object for database connection
 mysql = MySQL(app)
 
+#Homepage
 @app.route("/")
 def index():
 
+    #return index template as response
     return render_template("index.html")
 
+#about page
 @app.route("/about")
 def about():
 
+    #return about page as response
     return render_template("about.html")
 
-@app.route("/article/<string:id>")
-def detail(id):
-
-    return "Article id:" + id
 
 if __name__ == "__main__":
 
